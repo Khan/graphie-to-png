ffa499a0e690d8e741b2a8dc99e545cf9228d7ec
Ben Christel <benchristel@khanacademy.org>
2023-11-30 14:46:16 -0800

    Switch the math renderer from KaTeX to MathJax
    This is accomplished by adapting the MathJaxRenderer interface to look
    like KaTeX, since that's what Khan Exercises expects.
    
    Issue: https://khanacademy.atlassian.net/browse/LC-1002
    
    Test plan:
    
    - Start the dev server: `./server.sh`.
    - Visit http://localhost:8765
    - View all the example graphies by selecting them from the dropdown.
      They should all look pretty much the same as the renderings on
      production (http://graphie-to-png.khanacademy.systems/)

---
+++ static/js/mathjax-adapter.js
@@ -0,0 +1,19 @@
+// Adapt MathJax to look like KaTeX, so Khan Exercises can use it
+
+const renderer = new window.MathJaxRenderer({
+    fontURL: "https://cdn.kastatic.org/fonts/mathjax",
+    shouldFixUnicodeLayout: true,
+});
+
+window.katex = {
+    render(tex, container) {
+        // We ignore the error returned from renderer.render(), since Khan
+        // Exercises can't do anything to recover from it if we throw. If
+        // there is an error, domElement will still be non-null; it will
+        // contain the malformed TeX or an error message.
+        const {domElement} = renderer.render(tex);
+        container.innerHTML = "";
+        container.appendChild(domElement);
+        renderer.updateStyles();
+    }
+};
